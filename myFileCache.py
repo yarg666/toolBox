2f785205d12b39eefc73d117dab8021122995efb
yarg666 <30553714+yarg666@users.noreply.github.com>
2017-07-30 19:58:12 +0200

    Update myFileCache.py

--- myFileCache.py
+++ myFileCache.py
@@ -5,15 +5,18 @@ out= hou.node("/out")
 
 for node in nodeSelect:
     parent = hou.parent()
-    print parent
+    parentString = parent.name()      
     getName = node.name()
     connectNode = node.outputs()
     outNull = node.createOutputNode("null",getName.upper())
     outNull.setPosition(node.position())
     outNull.move([0, -.75])
     outNull.setColor(black)
-    myfile = outNull.createOutputNode("file",getName.upper()+"_CACHE")
-    myfile.setColor(pink)
+    #set read node to read myWriteGeo
+    myFile = outNull.createOutputNode("file",getName.upper()+"_CACHE")
+    myFile.setColor(pink)
+    myFile.setParms({"file": "$"+"HIP/geo/"+"$"+"HIPNAME."+"$"+"OS."+"$"+"F.bgeo.sc"})
+    #set myWriteGeo to cache the myFile
     myWriteGeo= out.createNode("geometry",getName.upper()+"_CACHE")
-    myWriteGeo.setParms({"soppath":"/obj/"+getName.upper()})
- 
+    myWriteGeo.setParms({"soppath":"/obj/"+parentString+"/"+getName.upper()})
+    myWriteGeo.setParms({"trange":"normal"})
