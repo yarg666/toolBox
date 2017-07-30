8ef7336ace5098d260429fd19b297adff6d969de
yarg666 <30553714+yarg666@users.noreply.github.com>
2017-07-30 10:27:32 +0200

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,5 +1,11 @@
 
 obj = hou.node("/obj")
+
 cam = obj.createNode("cam","myCam")
 cam.setParms({"resx":1920,"resy":1080})
 cam.setParms({"tz":3,"ty":1.7})
+
+out = hou.node("/out")
+
+mantra = out.createNode("ifs","myMantra")
+mantra.setParms({""})
