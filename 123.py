23d6285978c6d8e2fe1b1941aa598e3aa6ce2d32
yarg666 <30553714+yarg666@users.noreply.github.com>
2017-08-05 12:43:00 +0200

    Update 123.py

--- 123.py
+++ 123.py
@@ -5,13 +5,14 @@ cam.setParms({"resx":1920,"resy":1080})
 cam.setParms({"tz":3,"ty":1.7})
 
 geo1 = obj.createNode ("geo","myGeo")
+geo1.move([0, -2])
 file1 = hou.node("/obj/myGeo/file1")
 file1.destroy()
 
   
 env = obj.createNode("envlight","myEnv")
 env.setParms ({"env_map":"$HFS/houdini/pic/DOSCH_SKIESV2_01SN_lowres.rat"})
-env.move([0, 0.75])
+env.move([0, 1])
 
 out = hou.node("/out")
 mantra = out.createNode("ifd","myMantra")
