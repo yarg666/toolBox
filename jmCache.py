9fe6af720f4a1433250c4ef95a937b36c9fc1dd1
yarg666 <30553714+yarg666@users.noreply.github.com>
2017-09-10 21:11:03 +0200

    Update jmCache.py

--- jmCache.py
+++ jmCache.py
@@ -1,26 +1,61 @@
+def bound ():
+    import hou
+    nodeSelect = hou.selectedNodes()
+    black=hou.Color((0,0,0))
+    pink=hou.Color((0.9,0.304,0.9))
+    #out= hou.node("/out")
 
-def cache ()
+    for node in nodeSelect:
+        #parent = hou.node("..")
+        #parentString =parent.name()    
+        getName = node.name()
+        #connectNode = node.outputs()
+        outNull = node.createOutputNode("null",getName+"_IN_BOUND")
+        outNull.setPosition(node.position())
+        outNull.move([0, -1])
+        outNull.setColor(black)
+        #set read node to read myWriteGeo
+        outBound= outNull.createOutputNode("null",getName+"_BOUND_CENTROID")
+        outBound.setColor(pink)
+        #add create param to outBound
+        parm_group = outBound.parmTemplateGroup()
+        parm_folder = hou.FolderParmTemplate("folder","Bound_Centroid")
+        parm_folder.addParmTemplate(hou.FloatParmTemplate("bound","Bound",3))
+        parm_folder.addParmTemplate(hou.FloatParmTemplate("centroid","Centroid",3))
+        parm_group.append(parm_folder)
+        outBound.setParmTemplateGroup(parm_group)
+        outBound.setParms({"boundx": hou.hscriptStringExpression('bbox("../",D_XSIZE)')})
 
-	import hou
-	nodeSelect = hou.selectedNodes()
-	black=hou.Color((0,0,0))
-	pink=hou.Color((0.9,0.304,0.9))
-	out= hou.node("/out")
 
-	for node in nodeSelect:
-	    parent = hou.parent()
-	    parentString = parent.name()      
-	    getName = node.name()
-	    connectNode = node.outputs()
-	    outNull = node.createOutputNode("null",getName.upper())
-	    outNull.setPosition(node.position())
-	    outNull.move([0, -.75])
-	    outNull.setColor(black)
-	    #set read node to read myWriteGeo
-	    myFile = outNull.createOutputNode("file",getName.upper()+"_CACHE")
-	    myFile.setColor(pink)
-	    myFile.setParms({"file": "$"+"HIP/geo/"+"$"+"HIPNAME."+"$"+"OS."+"$"+"F.bgeo.sc"})
-	    #set myWriteGeo to cache the myFile
-	    myWriteGeo= out.createNode("geometry",getName.upper()+"_CACHE")
-	    myWriteGeo.setParms({"soppath":"/obj/"+parentString+"/"+getName.upper()})
-	    myWriteGeo.setParms({"trange":"normal"})
+def cache ():
+    import hou
+    nodeSelect = hou.selectedNodes()
+    black=hou.Color((0,0,0))
+    pink=hou.Color((0.9,0.304,0.9))
+    out= hou.node("/out")
+
+    for node in nodeSelect:
+        parent = hou.node("..")
+        parentString =parent.name()    
+        getName = node.name()
+        connectNode = node.outputs()
+        outNull = node.createOutputNode("null",getName.upper())
+        outNull.setPosition(node.position())
+        outNull.move([0, -.75])
+        outNull.setColor(black)
+        #set read node to read myWriteGeo
+        myFile = outNull.createOutputNode("file",getName.upper()+"_CACHE")
+        myFile.setColor(pink)
+        myFile.setParms({"file": "$"+"HIP/geo/"+"$"+"HIPNAME."+"$"+"OS."+"$"+"F.bgeo.sc"})
+        #set myWriteGeo to cache the myFile
+        myWriteGeo= out.createNode("geometry",getName.upper()+"_CACHE")
+        myWriteGeo.setParms({"soppath":"/obj/"+parentString+"/"+getName.upper()})
+        myWriteGeo.setParms({"trange":"normal"})
+
+
+
+
+
+
+
+
