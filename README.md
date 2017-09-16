d47b885b5e9d931d7734fd79304d84ecc1a1c705
yarg666 <yarg666@MacBook-Pro-de-yarg666.local>
2017-09-16 22:55:05 +0200

    pywy script begin pb with string multiline

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Hou script
-jm.recache // execute tout les file cache d'un file 
-jm.filecache // creer un file cache sans hipname
-jm.cache // creer une fct de version en fction du nom du script
+
 jm.pywi // code windows de tokeru
-jm.norm // normalize geo to the center of the scene
+
 jm.abc // cache abc jm.abc("$HIP/vfx/$OS/$OS")
+
+jm.cache // creer une fct de version en fction du nom du script
+
+jm.norm // normalize geo to the center of the scene
+
 
--- jm.py
+++ jm.py
@@ -1,3 +1,5 @@
+"""bound defini le bound box et le centroid d'un node"""
+
 def bound ():
     import hou
     nodeSelect = hou.selectedNodes()
@@ -28,6 +30,11 @@ def bound ():
         outBound.parm('centroidy').setExpression('centroid("../",D_Y)')
         outBound.parm('centroidz').setExpression('centroid("../",D_Z)')
 
+""" cache creer un rop output dans /out et le recharge dans le context d'origine
+cela permet d'enchainer les depandences dans /out et de les relirent automatiquement
+dans dans le contexte d'origine
+"""
+
 def cache ():
     import hou
     nodeSelect = hou.selectedNodes()
@@ -47,11 +54,56 @@ def cache ():
         #set read node to read myWriteGeo
         myFile = outNull.createOutputNode("file",getName.upper()+"_CACHE")
         myFile.setColor(pink)
-        myFile.setParms({"file": "$"+"HIP/geo/"+"$"+"HIPNAME."+"$"+"OS."+"$"+"F.bgeo.sc"})
+        myFile.setParms({"file": "$"+"HIP/geo/$OS/"+"$"+"HIPNAME."+"$"+"OS."+"$"+"F.bgeo.sc"})
         #set myWriteGeo to cache the myFile
         myWriteGeo= out.createNode("geometry",getName.upper()+"_CACHE")
         myWriteGeo.setParms({"soppath":"/obj/"+parentString+"/"+getName.upper()})
+        myWriteGeo.setParms({"sopoutput":"$HIP/geo/$OS/$HIPNAME.$OS.$F.bgeo.sc"})
         myWriteGeo.setParms({"trange":"normal"})
+
+""" filecache cree un super node filecache 
+qui ecrit la geo sans ce soucier du nom de fichier courant"""
+
+def filecache ():
+	import hou
+	nodeSelect = hou.selectedNodes()
+	pink=hou.Color ((0.9,0.304,0.9))
+
+	for node in nodeSelect:
+		getName = node.name()
+		myFileCache = node.createOutputNode("filecache",getName.upper()+"_CACHE")
+		myFileCache.setParms({"file":"$HIP/geo/$OS.$F.bgeo.sc"})
+		myFileCache.setParms({"loadfromdisk":True})
+		myFileCache.setColor(pink)
+
+def pywy ():
+    import hou
+    nodeSelect = hou.selectedNodes()
+    pink=hou.Color ((0.9,0.304,0.9))
+
+    for node in nodeSelect:
+        #create node from selected node
+        pyNull = node.createOutputNode("null","pythonRunCode")
+        pyNull.setColor(pink)
+        #prepa param
+        parm_group = pyNull.parmTemplateGroup()
+        parm_folder = hou.FolderParmTemplate("folder","pywy")
+        #button run code
+        button=hou.ButtonParmTemplate("runcode","Run_Code")
+        button.setTags({"script_callback_language":"python","script_callback":"exec(kwargs['node'].parm('parm').eval())"})
+        parm_folder.addParmTemplate(button)
+        #multistring
+        multistring=hou.StringParmTemplate("pythonCode","PythonCode",1)         
+        multistring.setTags({"editor":"1","editorlang":"python"})
+        parm_folder.addParmTemplate(multistring)
+        #append param
+        parm_group.append(parm_folder)
+        pyNull.setParmTemplateGroup(parm_group)
+
+
+
+
+
 
 
 
